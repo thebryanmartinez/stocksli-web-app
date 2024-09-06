37e6e3a41f64b1dd783738da91ff2474c5786a66
Bryan Martinez <bryanmartz972@gmail.com>
2024-09-06 10:26:06 -0600

    feat: 🎸 Implement aboslute imports to Vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      src: '/src',
+      '@assets': '/src/ui/assets',
+      '@components': '/src/ui/components',
+      '@pages': '/src/ui/pages',
+      '@hooks': '/src/ui/hooks',
+      '@domain': '/src/domain',
+      '@data': '/src/data'
+    }
+  }
 })
